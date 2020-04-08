0bab1e34ad3d16be8e4ea8db0c3b60c2e7efb5ea
wenwen829 <liuwnwn@gmail.com>
2020-04-08 13:32:01 -0400

    1st test commit

--- run_analysis.R
+++ run_analysis.R
@@ -1,2 +1,8 @@
 library(dplyr)
 library(tidyr)
+library(readr)
+if (!file.exists("./data")){dir.create("./data")}
+if(!file.exists("./data/Dataset.zip")){
+  download.file("https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip", 
+                destfile="./data/DataSet.zip")}
+if(!file.exists("./Data/UCI HAR Dataset")){unzip("./Data/Dataset.zip",exdir="./data")}
